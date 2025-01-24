65049c1c4d818330ad53d72775440c97b5b1d583
Arsal <a1706@192.168.0.104>
2025-01-25 03:25:21 +0500

    Project Added

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,83 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import bcrypt from "bcryptjs";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      // id: "credentials",
+      name: "Credentials",
+
+      credentials: {
+        identfier: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials: any): Promise<any> {
+        console.log(credentials, "IN SIGN IN::::");
+
+        await dbConnect();
+        try {
+          const user = await UserModel.findOne({
+            $or: [
+              { email: credentials.identifier },
+              { username: credentials.identifier },
+            ],
+          });
+
+          if (!user) {
+            throw new Error("User not found");
+          }
+          if (!user.isVerified) {
+            throw new Error(
+              "User not verified Please verify your account before login"
+            );
+          }
+          const isPasswordCorrect = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+
+          if (isPasswordCorrect) {
+            return user;
+          } else {
+            throw new Error("Invalid password please try again");
+          }
+        } catch (error: any) {
+          throw new Error(error);
+        }
+      },
+    }),
+  ],
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id?.toString();
+        token.isVerified = user.isVerified;
+        token.isAcceptingMessages = user.isAcceptingMessages;
+        token.username = user.username;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user._id = token._id;
+        session.user.isVerified = token.isVerified;
+        session.user.isAcceptingMessages = token.isAcceptingMessages;
+        session.user.username = token.username;
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/sign-in",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret:
+    "rw28VrgRfWHrE5Qhl8GElkEq9pPCviAjQ+gpV6Fl4FY=" ||
+    (process.env.NEXTAUTH_SECRET as string),
+};
---
+++ src/lib/resend.ts
@@ -0,0 +1,3 @@
+import { Resend } from "resend";
+
+export const resend = new Resend("re_T1nWSfdb_3aDTToKUnQHda2bb7v8kfc1k");
